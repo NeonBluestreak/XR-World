eff359a56baea00649dee822d3880e46f0af72a9
NeonBluestreak <66958662+NeonBluestreak@users.noreply.github.com>
2025-10-13 12:19:40 -0500

    VR world blockout/setup

---
+++ VRArt World/Assets/Scripts/AttackerRobotScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerRobotScript : MonoBehaviour
+{
+    //This Script needs to detect when a sword comes in contact with it and deplete health starting at 3 and going to 0
+    //At 0 the robot is destroyed
+    //Make sure that it's easy enough to play sound effects and change background music
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
